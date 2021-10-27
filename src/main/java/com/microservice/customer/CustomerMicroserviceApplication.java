357759ed1d2e64b4465dda69b697fecf462354a0
DiegoKraenauEveris <dkraenau@everis.com>
2021-10-27 11:39:51 -0500

    Add eureka client

--- src/main/java/com/microservice/customer/CustomerMicroserviceApplication.java
+++ src/main/java/com/microservice/customer/CustomerMicroserviceApplication.java
@@ -1,18 +1,12 @@
 package com.microservice.customer;
 
-import com.github.mongobee.Mongobee;
-import com.mongodb.ReadConcern;
-import com.mongodb.TransactionOptions;
-import com.mongodb.WriteConcern;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerMicroserviceApplication {
 
 
